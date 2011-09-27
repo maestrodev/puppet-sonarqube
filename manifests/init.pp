cbf2a556a4d39831e84c7e9e52e16061fcb28cdf
Carlos Sanchez <csanchez@maestrodev.com>
2011-09-27 20:35:45 +0200

    Add ASL license

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,3 +1,17 @@
+# Copyright 2011 MaestroDev
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 class sonar( $version = "2.10", $user = "sonar", $group = "sonar", $service = "sonar",
   $home = "/usr/local", $download_url = "http://dist.sonar.codehaus.org/sonar-$version.zip",
   $arch = "linux-x86-64" ) {
