9b8254328a6a9254340c1631107fb4cb7bc0f4ef
Carlos Sanchez <csanchez@maestrodev.com>
2013-08-08 17:41:01 +0200

    Fix warnings accessing instance variables in templates

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,1 +1,8 @@
 require 'puppetlabs_spec_helper/module_spec_helper'
+
+RSpec.configure do |c|
+  c.before(:each) do
+    Puppet::Util::Log.level = :warning
+    Puppet::Util::Log.newdestination(:console)
+  end
+end
