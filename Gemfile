41ae02988c955ff94565134af13e3b7334d7d10f
Carlos Sanchez <carlos@apache.org>
2015-03-09 14:43:26 +0100

    Run modulesync

--- Gemfile
+++ Gemfile
@@ -12,8 +12,6 @@ gem 'puppet-lint', '>= 1.1.0', :require => false
 gem 'simplecov', :require => false
 gem 'puppet-blacksmith', '>= 3.0.0', :require => false
 gem 'librarian-puppet', '>= 2.0.0', :require => false
-gem 'beaker', '>= 1.14.0', :require => false
-gem 'beaker-rspec', '>= 2.1.0', :require => false
-gem 'minitest', '< 5.0.0', :require => false
+gem 'beaker-rspec', '>= 3.0.0', :require => false
 
 # vim:ft=ruby
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,12 +5,27 @@ require 'puppetlabs_spec_helper/module_spec_helper'
 
 RSpec.configure do |c|
   c.treat_symbols_as_metadata_keys_with_true_values = true
+  c.mock_with :rspec
+  c.hiera_config = File.expand_path(File.join(__FILE__, '../fixtures/hiera.yaml'))
 
   c.before(:each) do
     Puppet::Util::Log.level = :warning
     Puppet::Util::Log.newdestination(:console)
   end
 
+  c.default_facts = {
+    :operatingsystem => 'CentOS',
+    :operatingsystemrelease => '6.6',
+    :kernel => 'Linux',
+    :osfamily => 'RedHat',
+    :architecture => 'x86_64',
+    :clientcert => 'puppet.acme.com'
+  }.merge({"http_proxy"=>nil, "maven_version"=>"3.0.5"})
+
+  c.before do
+    # avoid "Only root can execute commands as other users"
+    Puppet.features.stubs(:root? => true)
+  end
 end
 
 shared_examples :compile, :compile => true do
